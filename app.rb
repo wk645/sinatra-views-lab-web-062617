2d72130ae0979fd780a7c94934ebb4a5e48948e8
wk645 <wk645@nyu.edu>
2017-07-17 14:14:55 -0400

    erm

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get "/hello" do
+		erb :hello
+	end
+
+	get "/goodbye" do
+		erb :goodbye
+	end
+
+	get "/date" do
+		erb :date
+	end
 
 end
